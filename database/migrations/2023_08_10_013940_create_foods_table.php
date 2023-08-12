068b9f0cfbab00773e45cdc7909f768487ab5cb1
minhngoc211193 <ngocvtmgch211193@fpt.edu.vn>
2023-08-12 23:08:28 +0700

    Update 2023_08_10_013940_create_foods_table.php

--- database/migrations/2023_08_10_013940_create_foods_table.php
+++ database/migrations/2023_08_10_013940_create_foods_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('foods', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('type_id')->constrained('types');
+            $table->foreignId('type_id')->constrained()->onDelete('cascade');
             $table->string('name', 100);
             $table->string('description', 255);
             $table->string('price', 255);
